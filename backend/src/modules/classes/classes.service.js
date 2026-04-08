78102d962b694e44133f6cfd6c1e68eea82a6ae6
Wyclifitoh <wyclifitoh001@gmail.com>
2026-04-08 11:08:20 +0300

    update

--- backend/src/modules/classes/classes.service.js
+++ backend/src/modules/classes/classes.service.js
@@ -1,10 +1,12 @@
-const repo = require('./classes.repository');
+const repo = require("./classes.repository");
 
-const listClasses = async (schoolId, pagination) => repo.findAllClasses(schoolId, pagination);
+const listClasses = async (schoolId, pagination) =>
+  repo.findAllClasses(schoolId, pagination);
 const getClass = async (id, schoolId) => repo.findClassById(id, schoolId);
 const createClass = async (data) => repo.createClass(data);
 const listGrades = async (schoolId) => repo.findAllGrades(schoolId);
-const listStreams = async (schoolId, gradeId) => repo.findAllStreams(schoolId, gradeId);
+const listStreams = async (schoolId, gradeId) =>
+  repo.findAllStreams(schoolId, gradeId);
 const createGrade = async (data) => repo.createGrade(data);
 const createStream = async (data) => repo.createStream(data);
 const listSubjects = async (schoolId) => repo.findAllSubjects(schoolId);
@@ -15,8 +17,17 @@ const createDepartment = async (data) => repo.createDepartment(data);
 const listDesignations = async (schoolId) => repo.findAllDesignations(schoolId);
 
 module.exports = {
-  listClasses, getClass, createClass,
-  listGrades, listStreams, createGrade, createStream,
-  listSubjects, createSubject,
-  listStaff, listDepartments, createDepartment, listDesignations
+  listClasses,
+  getClass,
+  createClass,
+  listGrades,
+  listStreams,
+  createGrade,
+  createStream,
+  listSubjects,
+  createSubject,
+  listStaff,
+  listDepartments,
+  createDepartment,
+  listDesignations,
 };
